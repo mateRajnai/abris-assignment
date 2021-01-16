932a785d27f31ec811fc331f89c2d3e13881ba4f
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 17:40:20 +0100

    Create empty IssueService

--- src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
+++ src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
@@ -1,7 +1,7 @@
 package mate.rajnai.planningpoker.controller;
 
 import lombok.RequiredArgsConstructor;
-import mate.rajnai.planningpoker.repository.UserRepository;
+import mate.rajnai.planningpoker.service.IssueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IssueController {
 
 
-    private final UserRepository users;
+    private final IssueService issueService;
 
     @GetMapping
     public String getIssuesOfUser(@PathVariable String userId, Model model) {
---
+++ src/main/java/mate/rajnai/planningpoker/service/IssueService.java
@@ -0,0 +1,11 @@
+package mate.rajnai.planningpoker.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class IssueService {
+
+
+}
