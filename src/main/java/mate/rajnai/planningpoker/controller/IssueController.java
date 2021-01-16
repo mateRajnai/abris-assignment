cc95d956ccc46e55421a13619581ff871f0241cb
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 18:29:03 +0100

    Add returned issues of user to ui model attribute

--- src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
+++ src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
@@ -1,12 +1,15 @@
 package mate.rajnai.planningpoker.controller;
 
 import lombok.RequiredArgsConstructor;
+import mate.rajnai.planningpoker.model.Issue;
 import mate.rajnai.planningpoker.service.IssueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,6 +21,8 @@ public class IssueController {
 
     @GetMapping
     public String getIssuesOfUser(@PathVariable String userId, Model model) {
+        List<Issue> issuesOfUser = this.issueService.getIssuesOfUser(userId);
+        model.addAttribute("issuesOfUser", issuesOfUser);
         return "issues-of-user";
     }
 
