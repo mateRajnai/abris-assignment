dd4034604a599c162a94f9af41504f26637face9
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 23:47:32 +0100

    Return with List<IssueEstimation>

--- src/main/java/mate/rajnai/planningpoker/service/IssueService.java
+++ src/main/java/mate/rajnai/planningpoker/service/IssueService.java
@@ -1,13 +1,11 @@
 package mate.rajnai.planningpoker.service;
 
 import lombok.RequiredArgsConstructor;
-import mate.rajnai.planningpoker.model.Issue;
 import mate.rajnai.planningpoker.model.IssueEstimation;
 import mate.rajnai.planningpoker.repository.IssueEstimationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,12 +14,8 @@ public class IssueService {
     private final IssueEstimationRepository issueEstimations;
 
 
-    public List<Issue> getIssuesOfUser(String userId) {
-        List<IssueEstimation> issueEstimations = this.issueEstimations.findIssueEstimationByUserId(Long.valueOf(userId));
-        return issueEstimations
-                .stream()
-                .map(IssueEstimation::getIssue)
-                .collect(Collectors.toList());
+    public List<IssueEstimation> getIssuesOfUser(String userId) {
+        return this.issueEstimations.findIssueEstimationByUserId(Long.valueOf(userId));
     }
 
     public void estimateIssue(String userId, String issueId, String estimation) {
