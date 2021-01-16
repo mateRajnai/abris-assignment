2ebc29004e3e89b1f8e7c655d1dd739a21885952
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 18:27:05 +0100

    Get issues of user in IssueService

--- src/main/java/mate/rajnai/planningpoker/service/IssueService.java
+++ src/main/java/mate/rajnai/planningpoker/service/IssueService.java
@@ -1,11 +1,24 @@
 package mate.rajnai.planningpoker.service;
 
 import lombok.RequiredArgsConstructor;
+import mate.rajnai.planningpoker.model.Issue;
+import mate.rajnai.planningpoker.model.IssueEstimation;
+import mate.rajnai.planningpoker.repository.IssueEstimationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class IssueService {
 
+    private final IssueEstimationRepository issueEstimations;
 
+
+    public List<Issue> getIssuesOfUser(String userId) {
+        List<IssueEstimation> issueEstimations = this.issueEstimations.findIssueEstimationByUserId(Long.valueOf(userId));
+        List<Issue> issuesOfUser = issueEstimations.stream().map(IssueEstimation::getIssue).collect(Collectors.toList());
+        return issuesOfUser;
+    }
 }
