5525acd039d87da6121d4b47a50cda44af72ed44
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 14:52:12 +0100

    Add fields to Issue pojo

--- src/main/java/mate/rajnai/planningpoker/model/Issue.java
+++ src/main/java/mate/rajnai/planningpoker/model/Issue.java
@@ -1,4 +1,12 @@
 package mate.rajnai.planningpoker.model;
 
+import java.util.Set;
+
 public class Issue {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Set<IssueEstimation> estimations;
+    private int averageEstimation;
 }
