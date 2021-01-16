f0b1b6a4f39462a54aec91c07070b744aa45be07
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 17:19:24 +0100

    Create IssueController

---
+++ src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
@@ -0,0 +1,12 @@
+package mate.rajnai.planningpoker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/users/{userId}/issues")
+public class IssueController {
+
+
+
+}
