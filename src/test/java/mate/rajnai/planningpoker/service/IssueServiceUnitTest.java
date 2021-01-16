ad3e4b24d482b7c9ecf96854d0d78885ba3e218a
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 20:22:45 +0100

    Remove not used imports

--- src/test/java/mate/rajnai/planningpoker/service/IssueServiceUnitTest.java
+++ src/test/java/mate/rajnai/planningpoker/service/IssueServiceUnitTest.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
