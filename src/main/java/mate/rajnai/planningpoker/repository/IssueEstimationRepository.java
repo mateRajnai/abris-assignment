989ae3f361913e2988b7e17bddc917e9136ae3eb
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 18:27:51 +0100

    Create query method findIssueEstimationByUserId

--- src/main/java/mate/rajnai/planningpoker/repository/IssueEstimationRepository.java
+++ src/main/java/mate/rajnai/planningpoker/repository/IssueEstimationRepository.java
@@ -4,5 +4,11 @@ import mate.rajnai.planningpoker.model.IssueEstimation;
 import mate.rajnai.planningpoker.model.IssueEstimationKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueEstimationRepository extends JpaRepository<IssueEstimation, IssueEstimationKey> {
+
+    List<IssueEstimation> findIssueEstimationByUserId(Long userId);
+
+
 }
