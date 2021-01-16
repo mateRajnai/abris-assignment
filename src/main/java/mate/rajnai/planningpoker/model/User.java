0f9e7c0eef33226ddf754c0e9c674b407c58f7e5
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 14:38:53 +0100

    Add fields to User

--- src/main/java/mate/rajnai/planningpoker/model/User.java
+++ src/main/java/mate/rajnai/planningpoker/model/User.java
@@ -1,4 +1,15 @@
 package mate.rajnai.planningpoker.model;
 
+import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
 public class User {
+
+    private Long id;
+    private String name;
+    private Set<Issue> issues = new HashSet<>();
+    private Set<IssueEstimation> estimations = new HashSet<>();
 }
