6ad1045f5ec5347e4da4070e345cc4205a950ba0
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 16:55:22 +0100

    Create empty UserRepository test

---
+++ src/test/java/mate/rajnai/planningpoker/repository/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package mate.rajnai.planningpoker.repository;
+
+import mate.rajnai.planningpoker.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+
+
+}
