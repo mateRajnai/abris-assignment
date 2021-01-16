4a0b1017c6863842bc4bb2e09047a0f5b7e57d84
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 15:08:36 +0100

    Modify User table name

--- src/main/java/mate/rajnai/planningpoker/model/User.java
+++ src/main/java/mate/rajnai/planningpoker/model/User.java
@@ -3,16 +3,14 @@ package mate.rajnai.planningpoker.model;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "`user`")
 public class User {
 
     @Id
