8c58f7c3bd84ad23181a3d961da6833025290a57
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 14:54:59 +0100

    IssueEstimationKey implements Serializable

--- src/main/java/mate/rajnai/planningpoker/model/IssueEstimationKey.java
+++ src/main/java/mate/rajnai/planningpoker/model/IssueEstimationKey.java
@@ -5,12 +5,13 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode
 @Embeddable
-public class IssueEstimationKey {
+public class IssueEstimationKey implements Serializable {
 
     private Long userId;
     private Long issueId;
