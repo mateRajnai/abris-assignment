d2ac76357fb2faee668aa3cb1637d8aad7423dc8
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 21:26:56 +0100

    Create findIssueEstimationByUserIdAndIssueId in repo

--- src/main/java/mate/rajnai/planningpoker/repository/IssueEstimationRepository.java
+++ src/main/java/mate/rajnai/planningpoker/repository/IssueEstimationRepository.java
@@ -5,10 +5,13 @@ import mate.rajnai.planningpoker.model.IssueEstimationKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IssueEstimationRepository extends JpaRepository<IssueEstimation, IssueEstimationKey> {
 
     List<IssueEstimation> findIssueEstimationByUserId(Long userId);
+
+    Optional<IssueEstimation> findIssueEstimationByUserIdAndIssueId(Long userId, Long issueId);
 
 
 }
