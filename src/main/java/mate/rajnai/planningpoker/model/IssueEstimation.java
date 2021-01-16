6304cc5249b609a517c167fdc5b7c162c52e1bdc
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 14:49:34 +0100

    Create IssueEstimation pojo with fields

--- src/main/java/mate/rajnai/planningpoker/model/IssueEstimation.java
+++ src/main/java/mate/rajnai/planningpoker/model/IssueEstimation.java
@@ -1,4 +1,11 @@
 package mate.rajnai.planningpoker.model;
 
+
+
 public class IssueEstimation {
+
+    private IssueEstimationKey issueEstimationKey;
+    private User user;
+    private Issue issue;
+    private int estimation;
 }
