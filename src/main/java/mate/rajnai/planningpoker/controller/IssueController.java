0476cdb128f48961eabb7dbbe415fad9d68bcac5
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-16 17:31:07 +0100

    Create api endpoint of get issues of user

--- src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
+++ src/main/java/mate/rajnai/planningpoker/controller/IssueController.java
@@ -1,11 +1,25 @@
 package mate.rajnai.planningpoker.controller;
 
+import lombok.RequiredArgsConstructor;
+import mate.rajnai.planningpoker.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/users/{userId}/issues")
 public class IssueController {
+
+
+    private final UserRepository users;
+
+    @GetMapping
+    public String getIssuesOfUser(@PathVariable String userId, Model model) {
+        return "issues-of-user";
+    }
 
 
 
