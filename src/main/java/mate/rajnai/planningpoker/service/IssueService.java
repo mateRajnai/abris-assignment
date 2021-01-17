9afd17dd52c8a438264f681dd20a4ea8aa45b940
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-17 12:31:33 +0100

    Calculate average fibonacci estimation and save in issue

--- src/main/java/mate/rajnai/planningpoker/service/IssueService.java
+++ src/main/java/mate/rajnai/planningpoker/service/IssueService.java
@@ -8,6 +8,7 @@ import mate.rajnai.planningpoker.repository.IssueRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -23,8 +24,34 @@ public class IssueService {
 
     public void estimateIssue(String userId, String issueId, String estimation) {
         IssueEstimation issueEstimation = this.issueEstimations.findIssueEstimationByUserIdAndIssueId(Long.valueOf(userId), Long.valueOf(issueId)).orElseThrow(NullPointerException::new);
-        issueEstimation.setEstimation(Integer.parseInt(estimation));
+        int newEstimation = Integer.parseInt(estimation);
+        issueEstimation.setEstimation(newEstimation);
         this.issueEstimations.save(issueEstimation);
+
+        Issue issue = issueEstimation.getIssue();
+        Set<IssueEstimation> issueEstimations = issue.getEstimations();
+        issue.setAverageEstimation(this.getAverageOfFibonacciEstimations(issueEstimations));
+        this.issueRepository.save(issue);
+    }
+
+    private int getAverageOfFibonacciEstimations(Set<IssueEstimation> issueEstimations) {
+        int[] possibleEstimations = new int[]{1, 2, 3, 5, 8, 13};
+        int sumOfEstimations = issueEstimations
+                .stream()
+                .map(IssueEstimation::getEstimation)
+                .mapToInt(Integer::intValue)
+                .sum();
+        int numberOfEstimations = Math.toIntExact(issueEstimations.stream().filter(issueEstimation -> issueEstimation.getEstimation() != 0).count());
+        int normalAverage = sumOfEstimations / numberOfEstimations;
+        for (int i = 0; i < possibleEstimations.length - 1; i++) {
+            if (possibleEstimations[i] == normalAverage) return possibleEstimations[i];
+            if (possibleEstimations[i+1] == normalAverage) return possibleEstimations[i+1];
+            if (possibleEstimations[i] < normalAverage && normalAverage < possibleEstimations[i+1])
+                return possibleEstimations[i] - normalAverage >= possibleEstimations[i + 1] - normalAverage
+                        ? possibleEstimations[i + 1]
+                        : possibleEstimations[i];
+        }
+        return 0;
     }
 
     public List<Issue> getAllIssues() {
