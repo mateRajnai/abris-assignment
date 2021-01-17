3e1a38923b22febd64092d3bf09b3895e3dce2e7
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-17 11:52:19 +0100

    Create getAllIssues api endpoint

--- src/main/java/mate/rajnai/planningpoker/controller/AllIssueController.java
+++ src/main/java/mate/rajnai/planningpoker/controller/AllIssueController.java
@@ -1,11 +1,28 @@
 package mate.rajnai.planningpoker.controller;
 
+import lombok.RequiredArgsConstructor;
+import mate.rajnai.planningpoker.model.Issue;
+import mate.rajnai.planningpoker.service.IssueService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/api/issues")
 public class AllIssueController {
+
+    private final IssueService issueService;
+
+    @GetMapping
+    public String getAllIssues(Model model) {
+        List<Issue> issues = this.issueService.getAllIssues();
+        model.addAttribute("issues", issues);
+        return "issues";
+    }
 
 
 }
